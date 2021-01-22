5838ff74e8dcc6cc265cd7b4e3650f11f8b22995
AB-FlatCode <info@flatcode.hr>
2021-01-22 01:24:04 +0100

    Homepage layout started

--- resources/views/page-home.blade.php
+++ resources/views/page-home.blade.php
@@ -5,10 +5,16 @@
 @section('content')
  <div class="portfolio__hero-section">
    <div class="container-xl">
-   <div class="hero-section__content d-flex align-items-center">
+   <div class="hero-section__content d-flex flex-column justify-content-center">
      <h2 class="heading__hero"><span>Pozdrav, ja sam Alen. Web developer</span>
      <span>iz Rijeke specijaliziran za dizajn i kodiranje</span>
       web stranica.</h2>
+
+      <div class="hero-section__content-text">
+        <p class="lead">Pogledajte neke od mojih nedavnih projekata kako biste
+          stekli uvid u proces, rezultate i iskustva klijenata.
+          </p>
+      </div>
    </div>
   </div>
  </div>
