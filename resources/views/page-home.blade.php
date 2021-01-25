b54cf87d910b67030f30ee305a16057ac90a48e9
AB-FlatCode <info@flatcode.hr>
2021-01-25 21:11:29 +0100

    Portfolio layout component started

--- resources/views/page-home.blade.php
+++ resources/views/page-home.blade.php
@@ -3,9 +3,10 @@
 @extends('layouts.app')
 
 @section('content')
- <div class="portfolio__hero-section">
+ <section class="portfolio__hero-section">
    <div class="container-xl">
    <div class="hero-section__content d-flex flex-column justify-content-center">
+    <h3 class="heading__hero--small">Portfolio  <span class="heading__hero--small-text">Izrada web stranica</span></h3>
      <h2 class="heading__hero"><span>Pozdrav, <span class="heading__hero-line">ja sam Alen.</span> Web developer</span>
      <span>iz Rijeke specijaliziran za dizajn i kodiranje</span>
       web stranica.</h2>
@@ -20,5 +21,56 @@
       </div>
    </div>
   </div>
+</section>
+
+<section class="portfolio-wrapp">
+ <div class="container-lg">
+   <ul class="portfolio-grid">
+     <li class="portfolio-grid__item portfolio-grid__item--tall">
+       <a href="" class="portfolio-grid__item-img img-link">
+          <img src="@asset("images/brandier-ex.jpg")" alt="">
+       </a>
+       <div class="portfolio-grid__item-desc">
+         <h3 class="portfolio-grid__item-title">Grad Kastav</h3>
+         <p class="portfolio-grid__item-excerpt">Razvoj službenih web stranica Grada Kastva</p>
+       </div>
+     </li>
+
+      <li class="portfolio-grid__item portfolio-grid__item--wide">
+       <a href="" class="portfolio-grid__item-img img-link">
+          <img src="@asset("images/kastav-ex.jpg")" alt="">
+       </a>
+       <div class="portfolio-grid__item-desc">
+         <h3 class="portfolio-grid__item-title">Brandier</h3>
+         <p class="portfolio-grid__item-excerpt">Izrada web stranice digitalne agencije</p>
+       </div>
+     </li>
+
+     <li class="portfolio-grid__item portfolio-grid__item--wide">
+       <a href="" class="portfolio-grid__item-img img-link">
+          <img src="@asset("images/kastav-ex.jpg")" alt="">
+       </a>
+       <div class="portfolio-grid__item-desc">
+         <h3 class="portfolio-grid__item-title">Grad Kastav</h3>
+         <p class="portfolio-grid__item-excerpt">Razvoj službenih web stranica Grada Kastva</p>
+       </div>
+     </li>
+
+      <li class="portfolio-grid__item portfolio-grid__item--tall">
+       <a href="" class="portfolio-grid__item-img img-link">
+          <img src="@asset("images/brandier-ex.jpg")" alt="">
+       </a>
+       <div class="portfolio-grid__item-desc">
+         <h3 class="portfolio-grid__item-title">Brandier</h3>
+         <p class="portfolio-grid__item-excerpt">Izrada web stranice digitalne agencije</p>
+       </div>
+     </li>
+   </ul>
+          
+  </div>
+</section>
+ 
+  
+   
  </div>
 @endsection
